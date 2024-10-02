864aeaa92502a76e39caa5f4b7cec38d7e513d73
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2024-10-02 17:27:02 +0300

    fix: change action statement for assume role policy

--- roles.tf
+++ roles.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role" "GithubActionsRole" {
             Principal = {
               Federated = "arn:aws:iam::166023593358:oidc-provider/token.actions.githubusercontent.com"
             }
-            Action = "sts:AssumeRole"
+            Action = "sts:AssumeRoleWithWebIdentity"
             Condition = {
                 StringEquals = {
                     "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
